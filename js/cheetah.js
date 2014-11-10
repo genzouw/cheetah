1a99fec5c5f292f627e9255e61707c0d6bbbc23d
genzouw <genzouw@gmail.com>
2014-11-10 18:17:08 +0900

    IE8,9で正常動作しない問題に対応

--- js/cheetah.js
+++ js/cheetah.js
@@ -1,4 +1,14 @@
 (function($) {
+	if ($.ajaxPrefilter) {
+		$.ajaxPrefilter(function (options, originalOptions, jqXHR) {
+			if(originalOptions.type.toLowerCase() == 'post'){
+				options.data = jQuery.param($.extend(originalOptions.data||{}, {
+					timeStamp: new Date().getTime()
+				}));
+			}
+		});
+	}
+
 	$.outputLeadTimes = function(productCode, options) {
 		var defaultOptions = {
 			"success": function( json, status ) {
@@ -10,12 +20,24 @@
 
 		$.ajax({
 			// url: "//s3-ap-northeast-1.amazonaws.com/daito-lead-time-info/products/" + productCode + ".json?_=" + (new Date()),
-			url: "//s3-ap-northeast-1.amazonaws.com/daito-lead-time-info/products/" + productCode + ".json",
+			url: "//s3-ap-northeast-1.amazonaws.com/daito-lead-time-info/products/update_date.json",
 			type: "GET",
 			dataType: "json",
-			"async":false,
-			"success":setting.success,
-			"error":setting.error
+			success: function( data, status ) {
+				var updateDate = data.update_date;
+				$.ajax({
+					// url: "//s3-ap-northeast-1.amazonaws.com/daito-lead-time-info/products/" + productCode + ".json?_=" + (new Date()),
+					url: "//s3-ap-northeast-1.amazonaws.com/daito-lead-time-info/products/" + productCode + ".json",
+					type: "GET",
+					dataType: "json",
+					success: function( data, status ) {
+						data["データ出力日時"] = updateDate;
+						setting.success( data, status );
+					},
+					error:setting.error
+				});
+			},
+			error: setting.error
 		});
 
 		return this;
